b2afda68c6d58ea9fc4be7a14f26ba13ee7d9dfd
sam <semuall0108@gmail.com>
2025-11-11 16:00:17 -0800

    Finished setting up the basic imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,24 @@
-import './App.css'
+// Library Imports
+import { Routes, Route } from "react-router-dom";
 
-import { Button } from './components/ui/button'
+// Style Imports
+import "./App.css";
+
+// Component Imports
+import { LoginPage }  from "./pages/LoginPage";
+import { SignupPage } from "./pages/SignupPage";
+import { HomePage }   from "./pages/HomePage";
 
 function App() {
-
-  return (
-    <>
-        <Button>Click me</Button>
-    </>
-  )
+    return (
+        <>
+            <Routes>
+                <Route path="/login"  element={<LoginPage />}  />
+                <Route path="/signup" element={<SignupPage />} />
+                <Route path="/home"   element={<HomePage />}   />
+            </Routes>
+        </>
+    );
 }
 
-export default App
+export default App;
