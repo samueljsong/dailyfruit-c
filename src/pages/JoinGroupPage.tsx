09edafa82cea32921e01c8e4dbf85fb410102028
sam <semuall0108@gmail.com>
2025-11-17 18:21:28 -0800

    Finished implementing Korean for the Create Group Form and started working on the Join Group Page

--- src/pages/JoinGroupPage.tsx
+++ src/pages/JoinGroupPage.tsx
@@ -1,9 +1,13 @@
+import { NavMenu } from "@/components/NavMenu"
 
 export const JoinGroupPage = () => {
     
     return(
-        <div>
-            <h1>Join GroupPage</h1>
+        <div className=" flex flex-col items-center min-h-screen">
+            <NavMenu/>
+            <div className=" flex flex-1 items-center justify-center w-full mb-[5%]">
+                <h1>Join GroupPage</h1>
+            </div>
         </div>
     )
 }
