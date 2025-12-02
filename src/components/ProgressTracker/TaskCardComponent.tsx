536e6ed01dcbde3814d7eb590f2ffb0c9be5279a
sam <semuall0108@gmail.com>
2025-12-01 17:28:02 -0800

    workiing on task component

--- src/components/ProgressTracker/TaskCardComponent.tsx
+++ src/components/ProgressTracker/TaskCardComponent.tsx
@@ -1,4 +1,6 @@
 // Import Combonents
+import { Badge } from "../ui/badge"
+
 import {   
     Card,
     CardAction,
@@ -13,16 +15,19 @@ import { Button } from "../ui/button"
 export const TaskCardComponent = () => {
 
     return (
-        <Card className=" w-full">
+        <Card className=" flex flex-col w-full cursor-pointer aspect-square">
             <CardHeader>
                 <CardTitle>Task Name</CardTitle>
-            </CardHeader>
-            <CardContent>
-                <CardDescription>
-                    Task Description
-                </CardDescription>
                 <CardAction>
-                    <Button>Start Task</Button>
+                    <Badge className=" bg-[#e2cdff]">Incomplete</Badge>
+                </CardAction>
+            </CardHeader>
+            <CardContent className=" flex flex-col grow justify-between">  
+                <CardDescription>
+                    Task Description. This is where we will write down a brief explanation on what the task is about.
+                </CardDescription>
+                <CardAction className=" flex w-full">
+                    <Button className=" w-full cursor-pointer bg-primary/80 hover:bg-[#33cc99]">Complete</Button>
                 </CardAction>
             </CardContent>
         </Card>
